fdfe0187f9e90737806b187e0b83f4c8c8596027
RANGA REDDY NUKALA <60821111+irangareddy@users.noreply.github.com>
2020-02-08 22:53:35 +0530

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by RANGA REDDY NUKALA on 07/02/20.
+//  Copyright © 2020 RANGA REDDY NUKALA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
