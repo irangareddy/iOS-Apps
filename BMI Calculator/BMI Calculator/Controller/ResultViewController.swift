280c42cdc0e1bf03b1c0d55d2226b308a244b39b
RANGA REDDY NUKALA <rangareddy@RANGAs-MacBook-Pro.local>
2020-03-22 14:36:39 +0530

    Initial Commit

---
+++ BMI Calculator/BMI Calculator/Controller/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by RANGA REDDY NUKALA on 22/03/20.
+//  Copyright © 2020 RANGA REDDY NUKALA. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ BMI Calculator/BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by RANGA REDDY NUKALA on 22/03/20.
+//  Copyright © 2020 RANGA REDDY NUKALA. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
