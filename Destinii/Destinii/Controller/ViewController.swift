c3fe376af0930dba161c579eaacfc12516842884
irangareddy <itsmerr17@gmail.com>
2020-04-14 17:35:58 +0530

    Desitnii VC Updated

--- Destinii/Destinii/Controller/ViewController.swift
+++ Destinii/Destinii/Controller/ViewController.swift
@@ -2,46 +2,8 @@
 //  ViewController.swift
 //  Destinii
 //
-//  Created by RANGA REDDY NUKALA on 19/03/20.
+//  Created by RANGA REDDY NUKALA on 14/04/20.
 //  Copyright © 2020 RANGA REDDY NUKALA. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-    
-    
-    @IBOutlet weak var storyLabel: UILabel!
-    
-    @IBOutlet weak var choice1Button: UIButton!
-    
-    @IBOutlet weak var choice2Button: UIButton!
-    
-    var storyBrain = StoryBrain()
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        updateUI()
-        
-    }
-
-    
-    @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
-        storyBrain.nextStory(userChoice: sender.currentTitle!)
-        
-        updateUI()
-                
-    }
-    
-    
-    func updateUI() {
-        storyLabel.text = storyBrain.getStory()
-        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
-        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
-    }
-    
-}
-
+import Foundation
